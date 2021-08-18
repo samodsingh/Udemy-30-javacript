b1fb868301bf50f335d0a27f577ee9550927ec9a
samod singh <samod0098@gmail.com>
2021-08-18 12:14:08 +0530

    Udemy-30

---
+++ script.js
@@ -0,0 +1,15 @@
+let age =15;
+// age >= 18 ? console.log('I like to drink wine 🥂'):
+// console.log('I like to drink water 💧');
+
+
+let drink = age >= 18 ? 'wine 🥃':'water 💧';
+console.log(drink);
+
+let drink2 ;
+if(age>= 18){
+    drink2 ='wine🥃';
+}else{
+    drink2 = 'water 💧'
+}
+console.log(drink2)
